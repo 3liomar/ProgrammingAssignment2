d074ff322c9ee343eb8a0234f22581b5ff1b49db
3liomar <eliomarfranco27@gmail.com>
2017-07-05 20:40:24 -0400

    This is the final version of my Job

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function hola
+## This first function cache the matrix and defines a list of functions to 
+## get and set the inverse of this matrix in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {            #function to grab de matrix in the cache
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x             #function to take de matrix from the cache
+        setInv <- function(Inversa) inv <<- Inversa     #function to grab the new inverse
+        getInv <- function() inv        #function to get de inverse from the cache
+        list(set = set, get = get, setInv = setInv, getInv = getInv) #This is the list... 
+                                                   # ... of functions that returns this function
 }
 
 
-## Write a short comment describing this function hola
+## in this function, it takes the matrix of the cache function and the list of function to get and set the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getInv()
+        if(!is.null(inv)) {             #if we have calculated the inverse jet, and it is in the cache, ... 
+                return(inv)             #... the function returns the value in the cache
+        }
+        
+        data <- x$get()                 # Else, we get the new matrix in the cache...
+        inv <- solve(data, ...)         # Calculate the inverse, ...
+        x$setInv(inv)                   # ... change the inverse in the cache
+        inv                             # show the calculated inverse
 }
+
+# to make this function works we have to call the function makeCacheMatrix inside cachesolve
+
+# I.E: cacheSolve(makeCacheMatrix("Write here a Square function"))
+
